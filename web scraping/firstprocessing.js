bc50b18db2c2cd68dea83ce5fc56f10ceb986c28
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-09-30 12:58:09 +0530

    processing the results description

--- web scraping/firstprocessing.js
+++ web scraping/firstprocessing.js
@@ -8,3 +8,13 @@ let jsdom = require("jsdom");
 let fs = require("fs");
 
 let args = minimist(process.argv);
+
+fs.readFile(args.source, "utf-8", function(err, data){
+    let dom = new jsdom.JSDOM(data);
+    let document = dom.window.document;
+    let descs = document.querySelectorAll("div.match-info > div.description");
+    // we will get all div's with class description whose parent class is a div with class match-info.
+    for(let i=0; i<descs.length; i++){
+        console.log(descs[i].textContent);
+    }
+})
