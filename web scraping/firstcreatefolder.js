536c10330fd1be8d89d9cfe88317e34a7df50478
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-10-04 14:59:05 +0530

    create folder

---
+++ web scraping/firstcreatefolder.js
@@ -0,0 +1,17 @@
+// npm install minimist
+// node firstcreatefolder.js --source=teams.json --dest=root
+
+let minimist = require("minimist");
+let fs = require("fs");
+let path = require("path");// to make folder paths, never append slashes yourself
+// use path.join
+
+let args = minimist(process.argv);
+
+let teamsJSON = fs.readFileSync(args.source, "utf-8");
+let teams = JSON.parse(teamsJSON);
+
+for(let i = 0; i<teams.length; i++){
+    let foldername = path.join(args.dest, teams[i].name);
+    fs.mkdirSync(foldername);
+}
