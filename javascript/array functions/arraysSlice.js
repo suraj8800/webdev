fa801682488f5ef895f86963cfb3251cd777f95f
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-12-12 20:05:54 +0530

    Update arraysSlice.js

--- javascript/array functions/arraysSlice.js
+++ javascript/array functions/arraysSlice.js
@@ -18,6 +18,33 @@ displayArray(na5);
 let na6 = arr.slice(1, -2);
 displayArray(na6);
 
+//shallow copy
+let o1 = {
+    age: 10
+};
+
+let o2 = {
+    age: 20
+};
+
+let o3 = {
+    age: 30
+};
+
+let anarr = [o1, o2, o3];
+displayObjectiveArray(anarr);
+
+let scopy = anarr.slice();
+displayObjectiveArray(scopy);
+
 function displayArray(arr){
     console.log(arr + " = " + arr.length);
+}
+
+function displayObjectiveArray(arr){
+    let str = "";
+    for(let i=0; i< arr.length; i++){
+        str += arr[i].age + ", ";
+    }
+    console.log(str +". ");
 }
