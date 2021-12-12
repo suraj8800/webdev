2a9c267200a7a741764571f4510f8fa4318dec05
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-12-12 14:12:51 +0530

    push function

---
+++ javascript/array functions/arraypush.js
@@ -0,0 +1,18 @@
+// push append one or more value at the end
+// push return the length of array
+let arr = [10, 20, 80, 100, 50];
+disaplayArray(arr);
+
+arr.push(1000);
+disaplayArray(arr);
+
+arr.push(2000, 3000, 4000);
+disaplayArray(arr);
+
+let rv = arr.push(5000, 6000);
+disaplayArray(arr);
+console.log(rv);
+
+function disaplayArray(arr){
+    console.log(arr + " = " + arr.length);
+}
