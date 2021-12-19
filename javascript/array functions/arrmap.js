2efc683a688fb57a07944554566dc945f908bbad
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-12-19 19:14:59 +0530

    map

--- javascript/array functions/arrmap.js
+++ javascript/array functions/arrmap.js
@@ -1,3 +1,8 @@
+// map is function it self 
+// map return new array equal in length to original array
+// map takes a input a callback function
+// map will call the callback function multiple times (once for each)
+// for each run of callback, map will pass v, i and original array
 let arr = [2, 5, 9, 8, 15, 11, 6];
 let sqarr = arr.map(function(v, i, oarr){
     return v*v;
