749d89070a142043de0f6888c7e3944e7465060b
suraj biswas <53648721+suraj8800@users.noreply.github.com>
2021-12-27 12:58:26 +0530

    update

--- javascript/array functions/24dec.js
+++ javascript/array functions/24dec.js
@@ -1,4 +1,6 @@
-(function(){
-    let timer = prompt("Enter the time in sec?");
-    setInterval(for(let i=1; i<10; i++){element.innerHTML}, )
-})
+function PowerFunction(obj){
+    if(typeof obj.exp !=='number'){
+        console.log("exp must be a number");
+        return null;
+    }
+}
